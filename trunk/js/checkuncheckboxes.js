335ded06b61ffa62803106b5b7a5d813be5e1ff1
awpcp <awpcp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-06 01:36:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/another-wordpress-classifieds-plugin@123274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/js/checkuncheckboxes.js
+++ trunk/js/checkuncheckboxes.js
@@ -19,3 +19,26 @@
     				else {document.mycats.elements[i].checked = 1;}
 				}
 			}
+
+
+		 	function CheckAllAds()
+			{
+				count = document.manageads.elements.length;
+    			for (i=0; i < count; i++)
+				{
+    				if(document.manageads.elements[i].checked == 1)
+    				{document.manageads.elements[i].checked = 0; }
+    				else {document.manageads.elements[i].checked = 1;}
+				}
+			}
+			function UncheckAll(){
+
+				count = document.manageads.elements.length;
+   				for (i=0; i < count; i++)
+				{
+
+   					if(document.manageads.elements[i].checked == 1)
+    				{document.manageads.elements[i].checked = 0; }
+    				else {document.manageads.elements[i].checked = 1;}
+				}
+			}
